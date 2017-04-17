b13e7abe5e5c3870cdcb071dd774aca6d291d829
Kevin Lewis <kevin@jellybabi.es>
2017-04-17 15:32:41 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,17 @@
+/*
+  HOW TO ADD YOUR OWN KIT TO THE LIST
+
+  1. Fork this repository into your own account
+  2. Edit this file (data.js) and add a new object (everything in between and including the {} brackets). Make sure there's a comma between each object!
+  3. Submit a pull request to the original repository
+*/
+
+var items = [
+  {
+    name: "Example item 1",
+    link: "https://www.example.com",
+    owner: "Owner",
+    location: "London, UK",
+    contact: "contact@example.com"
+  }
+];
