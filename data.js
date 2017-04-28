e9a056001480b51c2ac8cdf2e881f6d47b36638f
Kevin Lewis <phazonoverload@users.noreply.github.com>
2017-04-28 12:02:37 +0100

    Update data.js

--- data.js
+++ data.js
@@ -5,5 +5,19 @@ var items = [
     owner: "BrumHack",
     location: "Birmingham, UK",
     contact: "dan@brumhack.co.uk"
+  },
+  {
+    name: "10 4-way extension leads",
+    link: "https://www.amazon.co.uk/Status-13-Way-Extension-Socket/dp/B00HD78QPE/ref=sr_1_9?ie=UTF8&qid=1493377284&sr=8-9",
+    owner: "Hacksmiths",
+    location: "London, UK",
+    contact: "hacksmiths@gold.ac.uk"
+  },
+  {
+    name: "5 6-way extension leads",
+    link: "https://www.amazon.co.uk/PIFCO-6-Gang-Surge-Protected-Extension/dp/B0042JWX14/ref=sr_1_4?ie=UTF8&qid=1493377284&sr=8-4",
+    owner: "Hacksmiths",
+    location: "London, UK",
+    contact: "hacksmiths@gold.ac.uk"
   }
 ];
